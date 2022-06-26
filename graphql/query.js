f8e46cf2dbb02eeed5568b3746514be5089b9216
Francisco Borja <fborja1414@gmail.com>
2022-06-25 23:53:11 -0700

    removed graphql limit

--- graphql/query.js
+++ graphql/query.js
@@ -2,32 +2,28 @@ import gql from 'graphql-tag';
 
 export const BlogQuery = gql`
 query BlogQuery{
-  pagination: { limit: 100 }) 
-    blogs {
-      data{
-        id
-        attributes{
-          BlogTitle
-          Blog
-          Note
-          NoteText
-          BlogText
-          BlogDate
-          position
-          maxWidth
-          NoteColor
-          StartingImage{
-          data{
-            id
-            attributes{
-              url
-            }
+  blogs (pagination: {limit: 100 } ) {
+    data{
+      id
+      attributes{
+        BlogTitle
+        Blog
+        Note
+        NoteText
+        NoteColor
+        position
+        maxWidth
+        NoteColor
+        StartingImage{
+        data{
+          id
+          attributes{
+            url
           }
         }
-      
-      
-        }
       }
-  }
-  }
-  `
+      }
+    }
+}
+}
+`
